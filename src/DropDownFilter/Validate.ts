ebf1feb581efc80c966752ebe54d764902b96b23
Andries Smit <mr.andries.smit@gmail.com>
2017-11-16 16:50:50 +0100

    Allign all widget.xml

--- src/DropDownFilter/Validate.ts
+++ src/DropDownFilter/Validate.ts
@@ -9,8 +9,11 @@ export class Validate {
             if (filter.filterBy === "XPath" && !filter.constraint) {
                 errorMessage.push(`Filter position: {${index + 1 }} is missing XPath constraint`);
             }
+            if (filter.filterBy === "attribute" && !filter.attribute) {
+                errorMessage.push(`Filter position: {${index + 1 }} 'Attribute' is required`);
+            }
             if (filter.filterBy === "attribute" && !filter.attributeValue) {
-                errorMessage.push(`Filter position: {${index + 1 }} is missing a Value constraint`);
+                errorMessage.push(`Filter position: {${index + 1 }} 'Attribute value' is required`);
             }
         });
         if (props.filters.filter(filter => filter.isDefault).length > 1) {
