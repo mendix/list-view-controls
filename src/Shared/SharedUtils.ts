85531c2a49eef63625b9be51768bcb092acfd7cf
Derrick Kwagala <derrick.kwagala@gmail.com>
2018-02-02 19:08:23 +0300

    Review: Find the list view with correct entity

--- src/Shared/SharedUtils.ts
+++ src/Shared/SharedUtils.ts
@@ -1,3 +1,5 @@
+import * as dijitRegistry from "dijit/registry";
+
 export interface WrapperProps {
     class: string;
     style: string;
@@ -86,5 +88,33 @@ export class SharedUtils {
         }
 
         return targetNode;
+    }
+
+    static findTargetListView(filterNode: HTMLElement, listViewEntity?: string): ListView | undefined {
+        let targetListView: ListView | undefined;
+
+        while (filterNode) {
+            const targetNodes = filterNode.querySelectorAll(`.mx-listview`);
+
+            if (filterNode.isEqualNode(document) || !filterNode.classList || filterNode.classList.contains("mx-incubator")
+                || filterNode.classList.contains("mx-offscreen")) {
+                break;
+            }
+
+            for (let count = 0; count < targetNodes.length; count++) { //tslint:disable-line
+                targetListView = dijitRegistry.byNode(targetNodes.item(count) as HTMLElement);
+
+                if (targetListView && !listViewEntity) { // returns the first found list view
+                    return targetListView;
+                }
+
+                if (targetListView && (targetListView._entity === listViewEntity)) {
+                    return targetListView;
+                }
+            }
+            filterNode = filterNode.parentNode as HTMLElement;
+        }
+
+        return targetListView;
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -90,7 +90,7 @@ const previewConfig = {
         ]
     },
     devtool: "inline-source-map",
-    externals: [ "react", "react-dom" ],
+    externals: [ "react", "react-dom", /^mxui\/|^mendix\/|^dojo\/|^dijit\// ],
     plugins: [
         new webpack.LoaderOptionsPlugin({ debug: true })
     ]
