a962f538ef4f395efe937029dae90eeff7868aff
Derrick Kwagala <derrick.kwagala@gmail.com>
2018-05-08 20:30:31 +0300

    Fix DropDownSort end-to-end test

--- tests/e2e/DropDownSort/DropDownSort.spec.ts
+++ tests/e2e/DropDownSort/DropDownSort.spec.ts
@@ -7,6 +7,7 @@ describe("Drop down sort", () => {
         page.open();
         page.dropdownSort.waitForVisible();
         page.listViewFirstItem.waitForVisible();
+        browser.pause(1000);
 
         const itemValue = page.listViewFirstItem.getHTML();
         expect(itemValue).toContain(testValue);
