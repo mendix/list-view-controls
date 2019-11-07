63edfaa9347da19f3fd0efdbc04cd8e65ee70559
Andries Smit <Andries.Smit@mendix.com>
2019-11-07 14:45:20 +0100

    Review improve e2e

---
+++ tests/e2e/Pagination/pages/paginationCustom.page.ts
@@ -0,0 +1,35 @@
+class PaginationCustom {
+    public get paginationOne() { return $(".mx-name-pagination9"); }
+
+    public get listViewOne() { return $(".mx-name-listView1"); }
+
+    public get nextButton() { return $(".mx-name-pagination9 > div > button.btn.mx-button.mx-name-paging-next"); }
+
+    public get firstButton() { return $(".mx-name-pagination9 > div > button.btn.mx-button.mx-name-paging-first"); }
+
+    public get previousButton() { return $(".mx-name-pagination9 > div > button.btn.mx-button.mx-name-paging-previous"); }
+
+    public get customButtonTwo() { return $("#mxui_widget_ReactCustomWidgetWrapper_0 > div > ul > li:nth-child(2)"); }
+
+    public get lastButton() { return $(".mx-name-pagination9 > div > button.btn.mx-button.mx-name-paging-last"); }
+
+    public get listViewFirstItem() { return $(".mx-name-listView1 > ul > li.mx-listview-item.mx-name-index-0 > div > div > span"); }
+
+    public get listViewThirdItem() { return $(".mx-name-listView1 > ul > li.mx-listview-item.mx-name-index-2 > div > div > span"); }
+
+    public get listView4ThirdItem() { return $("ul > li.mx-listview-item.mx-name-index-2"); }
+
+    public get listViewFifthItem() { return $(".mx-name-listView1 > ul > li.mx-listview-item.mx-name-index-4 > div > div > span"); }
+
+    public get listViewSeventhItem() { return $(".mx-name-listView1 > ul > li.mx-listview-item.mx-name-index-6 > div > div > span"); }
+
+    public get listViewNinethItem() { return $(".mx-name-listView1 > ul > li.mx-listview-item.mx-name-index-8 > div > div > span"); }
+
+    public get listViewLastItem() { return $(".mx-name-listView1 > ul > li.mx-listview-item.mx-name-index-18 > div > div > span"); }
+
+    public open(): void {
+        browser.url("/p/pagination-custom");
+    }
+}
+const page = new PaginationCustom();
+export default page;
--- tests/e2e/TextSearch/pages/home.page.ts
+++ tests/e2e/TextSearch/pages/home.page.ts
@@ -1,6 +1,6 @@
 class HomePage {
     public get searchInput() { return $(".mx-name-text_boxSearch1 input"); }
-    public get searchButton() { return $(".mx-name-text_boxSearch1 button"); }
+    public get searchClearButton() { return $(".mx-name-text_boxSearch1 button"); }
     public get listViewList() { return $(".mx-name-listView3 .mx-listview-list"); }
     public get listViewListItems() { return $$(".mx-name-listView3 .mx-listview-item"); }
     public open(): void {
